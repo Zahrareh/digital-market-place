68bdd3f6a7845507e40c11d963b263ca596ac26e
Zahra.reh <zahra.reh1998@gmail.com>
2024-01-31 14:13:50 +0330

    Implement Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import MaxWidthWrapper from "./MaxWidthWrapper"
 import { Icons } from "./Icons"
+import NavItems from "./NavItems"
 
 const Navbar = ()=>{
     return(
@@ -9,13 +10,16 @@ const Navbar = ()=>{
             <MaxWidthWrapper>
                 <div className="border-b border-gray-200">
                     <div className=" flex h-16 items-center">
-                        {/* TODO: Mpbile nav */}
+                        {/* TODO: Mobile nav */}
 
 
                         <div className="ml-4 flex lg:ml-0 ">
                           <Link href="/">
-                            <Icons.logo />
+                            <Icons.logo className="h-10 w-10" />
                             </Link>  
+                        </div>
+                        <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch ">
+                            <NavItems/>
                         </div>
                     </div>
                 </div>
---
+++ src/config/index.tsx
@@ -0,0 +1,42 @@
+export const PRODUCT_CATEGORIES = [
+    {
+        lable: "UI kits",
+        value: "ui_kits" as const,
+        featured: [
+            {
+                name: "Editors picks",
+                href: "#",
+                iamgeSrc: "/nav/ui-kits/mixed.jpg",
+            },
+            {
+                name: "New Arrivels",
+                href: "#",
+                iamgeSrc: "/nav/ui-kits/blue.jpg",
+            },{
+                name: "Bestselleers",
+                href: "#",
+                iamgeSrc: "/nav/ui-kits/purple.jpg",
+            },
+        ]
+    },
+    {
+        lable: "Icons",
+        value: "icons" as const,
+        featured: [
+            {
+                name: "Favorite Icon Picks",
+                href: "#",
+                iamgeSrc: "/nav/ui-kits/picks.jpg",
+            },
+            {
+                name: "New Arrivels",
+                href: "#",
+                iamgeSrc: "/nav/ui-kits/new.jpg",
+            },{
+                name: "Bestselling Icons",
+                href: "#",
+                iamgeSrc: "/nav/ui-kits/bestsellers.jpg",
+            },
+        ]
+    }
+]
